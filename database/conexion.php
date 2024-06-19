bd91c8bea92495a96cfd3f430f0e62d3ee05f913
MIKE <al222010230@gmail.com>
2024-06-18 20:32:58 -0600

    depuracion de db y ajustes en registros preguntas(opcion , multi)

--- database/conexion.php
+++ database/conexion.php
@@ -1,7 +1,7 @@
 <?php
 // db.php
 $host = 'localhost';
-$db = 'encuesta001';
+$db = 'encuesta_01';
 $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
@@ -15,4 +15,18 @@ try {
 } catch (\Exception $e) {
     die("Error al conectar a la base de datos: " . $e->getMessage());
 }
+
+$sql = "SELECT pregunta_id, depende_de_pregunta_id FROM dependencias_preguntas";
+$result = $conexion->query($sql);
+
+$dependencias = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $dependencias[] = $row;
+    }
+}
+
+
+// echo json_encode($dependencias);
 ?>
